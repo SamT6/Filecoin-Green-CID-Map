f51d5c86f4552730182d446b3f71ba89f9cfcb0b
SamT6 <samueltang06@gmail.com>
2022-12-12 23:12:07 +0800

    routing + map library

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
+import { useParams, useNavigate } from 'react-router-dom';
 
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 
 const App = () => {
-  const [cid, setCID] = useState("");
+  const { cid } = useParams();
+  const navigate = useNavigate();
 
+  const [searchCID, setSearchCID] = useState("");
 
-  const handleSubmit = (event) => {
+  useEffect(()=>{
+    console.log("searching cid: ", cid);
+  }, [cid]);
+
+  const handleClickSearch = (event) => {
     event.preventDefault();
-
-    console.log(cid);
+    navigate(`/${searchCID}`);
   }
 
   return (
     <div>
       <h2 className="special-font">Filecoin Green CID Map</h2>
-      <div className="grid p-fluid">
+      <div className="grid p-fluid" style={{marginLeft: 50, marginRight: 50}}>
           <div className="col-12 md:col-4">
               <div className="p-inputgroup">
-                  <InputText placeholder="CID" value={cid} onChange={(e) => setCID(e.target.value)}/>
-                  <Button style={{backgroundColor: "#00BE52", border: "0px"}} label="Search" onClick={handleSubmit}/>
+                  <InputText placeholder="CID" value={searchCID} onChange={(e) => setSearchCID(e.target.value)}/>
+                  <Button style={{backgroundColor: "#00BE52", border: "0px"}} label="Search" onClick={handleClickSearch}/>
               </div>
           </div>
       </div>
-
-
-
+      <div style={{marginTop: 50}}>
+        <MapContainer style={{height: "500px"}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import App from './App';
 import "primereact/resources/themes/lara-light-indigo/theme.css";  
 import "primereact/resources/primereact.min.css";                  
 import "primeicons/primeicons.css";         
-import "./styles/index.css"                       
+import "./styles/index.css"            
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+        <Routes>
+            <Route exact path="/:cid" element={<App />} />
+            <Route path="*" element={<App />} />
+        </Routes>
+      </Router>
   </React.StrictMode>
 );
