3718f264a1754d87a4dd1e0e37797c74864a672d
SamT6 <samueltang06@gmail.com>
2022-12-23 23:53:12 +0800

    energy api + ui

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { Splitter, SplitterPanel } from 'primereact/splitter';
 import { Card } from 'primereact/card';
 import { ScrollPanel } from 'primereact/scrollpanel';
 import { Toast } from 'primereact/toast';
-
+import { Tag } from 'primereact/tag';
 
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
@@ -26,6 +26,10 @@ let DefaultIcon = L.icon({
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
+const corner1 = L.latLng(-90, -200)
+const corner2 = L.latLng(90, 200)
+const bounds = L.latLngBounds(corner1, corner2)
+
 
 const App = () => {
   const { cid } = useParams();
@@ -36,7 +40,10 @@ const App = () => {
   const [providers, setProviders] = useState([]);
   const [minerIds, setMinerIds] = useState([])
   const [peerIdToMinerId, setPeerIdToMinerId] = useState({});
-  const [minerIdToMinerInfo, setMinerIdToMinerInfo] = useState({});
+  const [minerIdToLocation, setMinerIdToLocation] = useState({});
+  const [minerIdToEnergy, setMinerIdToEnergy] = useState({});
+  const [totalEnergy, setTotalEnergy] = useState(0);
+  const [totalRenewableEnergy, setTotalRenewableEnergy] = useState(0);
 
   useEffect(()=>{
     console.log("searching cid: ", cid);
@@ -75,7 +82,9 @@ const App = () => {
               map[element.PeerId] = element.MinerId;
               list.push(element.MinerId);
             });
+            fetchMinerEnergy(list);
             fetchMinerLocations(list);
+            
             setPeerIdToMinerId(map);
             setMinerIds(list);
           })
@@ -84,10 +93,56 @@ const App = () => {
           });
   }
 
+  const fetchMinerEnergy = (miner_ids) => {
+    let minerEnergyInfo = {};
+    let totalEnergy = 0;
+    let totalRenewableEnergy = 0;
+
+    miner_ids.forEach(id => {
+      let minerInfo = {};
+      // get Total amount of data stored by minerID
+      axios.get(`https://api.filgreen.d.interplanetary.one/models/model?code_name=CapacityModel&filter=month&miner=${id}`)
+      .then(result => {
+        minerInfo["total_data"] = result.data.data[0].data.slice(-1)[0].value + " " + result.data.y;
+        // console.log("total data: ", minerInfo["total_data"]);
+      })
+
+      // get Lower bound, upper bound, and estimate of total energy used by this minerID
+      axios.get(`https://api.filgreen.d.interplanetary.one/models/model?code_name=CumulativeEnergyModel_v_1_0_1&filter=month&miner=${id}`)
+      .then(result => {
+        minerInfo["upper_bound_energy"] = result.data.data[2].data.slice(-1)[0].value + " " + result.data.y;
+        minerInfo["lower_bound_energy"] = result.data.data[0].data.slice(-1)[0].value + " " + result.data.y;
+        minerInfo["estimate_energy"] = result.data.data[1].data.slice(-1)[0].value + " " + result.data.y;
+        // console.log("upper bound: ", minerInfo["upper_bound_energy"]);
+        // console.log("lower bound: ", minerInfo["lower_bound_energy"]);
+        // console.log("estimate: ", minerInfo["estimate_energy"]);
+        console.log(parseFloat(result.data.data[1].data.slice(-1)[0].value))
+        totalEnergy += parseFloat(result.data.data[1].data.slice(-1)[0].value);
+        console.log(totalEnergy)
+        setTotalEnergy(totalEnergy);
+
+      })
+
+      // get Total amount of renewable energy purchased by this minerID
+      axios.get(`https://api.filgreen.d.interplanetary.one/models/model?code_name=RenewableEnergyModel&filter=month&miner=${id}`)
+      .then(result => {
+        minerInfo["renewable_energy"] = result.data.data[0].data.slice(-1)[0].value + " " + result.data.y;
+        // console.log("renewable: ", minerInfo["renewable_energy"]);
+        totalRenewableEnergy += parseFloat(result.data.data[0].data.slice(-1)[0].value);
+        setTotalRenewableEnergy(totalRenewableEnergy);
+      })
+
+      minerEnergyInfo[id] = minerInfo; 
+    });
+
+    console.log(minerEnergyInfo)
+    setMinerIdToEnergy(minerEnergyInfo);
+  }
+
   const fetchMinerLocations = (miner_ids) => {
     axios.get("https://provider-quest.s3.us-west-2.amazonaws.com/dist/geoip-lookups/synthetic-locations-latest.json")
       .then(result => {
-        console.log(result.data);
+        // console.log(result.data);
         let map = {};
         miner_ids.forEach(element => {
           map[element] = []
@@ -97,8 +152,8 @@ const App = () => {
             map[element.provider].push(element)
           }
         });
-        console.log(map)
-        setMinerIdToMinerInfo(map);
+        // console.log(map);
+        setMinerIdToLocation(map);
       })
       .catch(error => {
         console.log(error);
@@ -108,7 +163,7 @@ const App = () => {
   const handleClickSearch = (event) => {
     event.preventDefault();
     navigate(`/${searchCID}`);
-    fetchProviders()
+    fetchProviders();
   }
 
   const showError = () => {
@@ -118,10 +173,10 @@ const App = () => {
   const generateMinerLocationMarkers = () => {
     let components = [];
     minerIds.forEach(mid => {
-      if(minerIdToMinerInfo[mid]){
-        let list = minerIdToMinerInfo[mid].map(minerInfo => {
+      if(minerIdToLocation[mid]){
+        let list = minerIdToLocation[mid].map(minerInfo => {
           const position = [minerInfo.lat, minerInfo.long];
-          console.log(minerInfo);
+          //console.log(minerInfo);
 
           return(
             <Marker position={position}>
@@ -139,6 +194,59 @@ const App = () => {
     return components;
   }
 
+  const generateMinerInfoCards = () => {
+    return providers.map((provider, index) => {
+      let title = "Not Filecoin Miner"
+      if(peerIdToMinerId[provider.ID]){
+        const minerId = peerIdToMinerId[provider.ID];
+        title = `Miner ID: ${minerId}`
+
+        return(
+          <Card title={title} subTitle={`Peer ID: ${provider.ID}`} style={{margin: "10px"}} key={index}>
+            <div style={{wordBreak: "break-all"}}>
+              <p> Address(es): 
+                {provider.Addrs.reduce((accumulator, currentProvider, currentIndex) => {
+                    if(currentIndex === 0)
+                      return accumulator + currentProvider;
+                    else 
+                    return accumulator + ", " + currentProvider;
+                  }, "")
+                } 
+              </p>
+              <Tag value={"Total data stored: " + minerIdToEnergy[minerId]["total_data"]} severity="primary" style={{margin: "2px"}}></Tag>
+              <br></br>
+              <Tag value={"Estimated total energy used: " + minerIdToEnergy[minerId]["estimate_energy"]} severity="warning" style={{margin: "2px"}}></Tag>
+              <br></br>
+              <Tag value={"Lower-bound total energy used: " + minerIdToEnergy[minerId]["lower_bound_energy"]} severity="info" style={{margin: "2px"}}></Tag>
+              <br></br>
+              <Tag value={"Upper-bound total energy used: " + minerIdToEnergy[minerId]["upper_bound_energy"]} severity="info" style={{margin: "2px"}}></Tag>
+              <br></br>
+              <Tag value={"Total renewable energy purchased: " + minerIdToEnergy[minerId]["renewable_energy"]} severity="success" style={{margin: "2px"}}></Tag>
+            </div>
+          </Card>
+        )
+      }
+      else{
+        return(
+          <Card title={title} subTitle={`Peer ID: ${provider.ID}`} style={{margin: "10px"}} key={index}>
+            <div style={{wordBreak: "break-all"}}>
+              <p> Address(es): 
+                {provider.Addrs.reduce((accumulator, currentProvider, currentIndex) => {
+                    if(currentIndex === 0)
+                      return accumulator + currentProvider;
+                    else 
+                    return accumulator + ", " + currentProvider;
+                  }, "")
+                } 
+              </p>                      
+            </div>
+          </Card>
+        )
+      }
+
+    })
+  }
+
   return (
     <div>
       <h2 className="special-font">Filecoin Green CID Map</h2>
@@ -150,16 +258,18 @@ const App = () => {
               </div>
           </div>
       </div>
+      
+ 
 
       <Splitter style={{height: '550px', marginTop: "20px"}} layout="horizontal">
         <SplitterPanel size={55} minSize={30}>
           <div style={{margin: 10}}>
-            <MapContainer style={{height: "525px"}} center={[30, 0]} zoom={2} scrollWheelZoom={true}>
+            <MapContainer style={{height: "525px"}} center={[28, 8]} zoom={2} scrollWheelZoom={true} 
+                          maxBoundsViscosity={1.0} maxBounds={bounds}>
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
-              
               {
                 generateMinerLocationMarkers()
               }
@@ -168,22 +278,16 @@ const App = () => {
         </SplitterPanel>
         <SplitterPanel size={45} minSize={20}>
           <ScrollPanel style={{width: '100%', height: '100%'}} className="custom-handle">
+            <Card style={{margin: "5px", padding: "2px"}} >
+              <div>
+                  <p>Total energy ⚡️ used by all minerIDs storing the CID</p>
+                  <Tag value={totalEnergy + " kWh"} severity="warning" style={{margin: "2px"}}></Tag>
+                  <p>Total renewable energy 🌱 purchased by all minerIDs storing the CID</p>
+                  <Tag value={totalRenewableEnergy + " kWh"} severity="success" style={{margin: "2px"}}></Tag>
+              </div>
+            </Card>
             {
-              providers.map((provider, index) => {
-                let title = "Not Filecoin Miner"
-                if(peerIdToMinerId[provider.ID]){
-                  const minerId = peerIdToMinerId[provider.ID];
-                  title = `Miner ID: ${minerId}`
-                }
-
-                return(
-                  <Card title={title} subTitle={`Peer ID: ${provider.ID}`} style={{margin: "10px"}} key={index}>
-                    <div style={{wordBreak: "break-all"}}>
-                      <p> Address(es): {provider.Addrs} </p>
-                    </div>
-                  </Card>
-                )
-              })
+              generateMinerInfoCards()
             }
           </ScrollPanel>
         </SplitterPanel>
